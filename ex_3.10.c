944a225c2158e9e04ccca2bc1992d8caacad4e71
richa290903 <155316586+richa290903@users.noreply.github.com>
2024-01-01 09:47:52 +0530

    function

---
+++ ex_3.10.c
@@ -0,0 +1,19 @@
+// Exercise Question :calculate the square root of a number using a function
+#include <stdio.h>
+void squares(int);
+void squares(int n){
+    int i;
+    float x;
+    x=n;
+    for(i=0;i<=n;i++){
+        x=(x+n/x)/2;    //(4+4/4)/2=2.5     //(2.5+4/2.5)/2=2.05    //(2.05+4)/2=2.006      //(2.006+4/2.006)/2=2
+                        //4+1/2=2.5
+    }
+    printf("The squre root of number %f :%f",n,x);
+}
+int main(){
+    int n;
+    printf("Enter any number :");
+    scanf("%d",&n);
+    squares(n);
+}
